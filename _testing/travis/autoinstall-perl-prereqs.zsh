097ea77e54af0266b70844a23ee1f03224f5143a
Ailin Nemui <ailin@linux.site>
2015-01-17 18:26:31 +0100

    Make prereq-scanner more noisy
    Avoid stalled builds by outputting the prereq-scanner result.

--- _testing/travis/autoinstall-perl-prereqs.zsh
+++ _testing/travis/autoinstall-perl-prereqs.zsh
@@ -4,22 +4,26 @@
 
 local -a modlist
 modlist=($(scan-perl-prereqs $filelist))
+echo -n ... >&2
 
-sudo apt-file update >/dev/null 2>&1
+sudo apt-file update
 
 local -a ubu_pkgs
 local -a cpan_mods
 for mod ($modlist) {
     mod=${mod%\~*}
     if [[ $mod != Irssi* && $mod != feature ]] {
+        echo -n $mod >&2
         if { ! perl -M$mod -E1 2>/dev/null } {
             local -a ubu_pkg
             ubu_pkg=($(apt-file -l search "/perl5/${mod//:://}.pm"))
-            if [[ $#ubu_pkg -gt 0 ]] { ubu_pkgs+=($ubu_pkg) } \
+            if [[ $#ubu_pkg -gt 0 ]] { ubu_pkgs+=($ubu_pkg); echo -n '(u)' >&2 } \
             else { cpan_mods+=($mod) }
         }
+        echo -n ' ' >&2
     }
 }
+echo >&2
 
 if [[ $#ubu_pkgs -gt 0 ]] { sudo apt-get install -qq $ubu_pkgs }
 
@@ -29,6 +33,7 @@ typeset -A broken_mods
 broken_tests=($(perl -MYAML::Tiny=LoadFile -e'print "$_ 1 " for @{LoadFile(+shift)->{cpan}{broken_tests}}' _testing/config.yml))
 broken_mods=($(perl -MYAML::Tiny=LoadFile -e'print "$_ 1 " for @{LoadFile(+shift)->{cpan}{broken_modules}}' _testing/config.yml))
 
+echo ... >&2
 for mod ($cpan_mods) {
     if { ! perl -M$mod -E1 2>/dev/null } {
         local skip_test=
