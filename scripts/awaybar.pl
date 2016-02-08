a39c5878902db9958ed73d5c192d341b83fe9419
Dreamcat4 <dreamcat4@gmail.com>
2016-02-08 13:14:55 +0000

    Fix appearance / user formatting of awaybay
    source: https://github.com/ronilaukkarinen/weed

--- scripts/awaybar.pl
+++ scripts/awaybar.pl
@@ -1,6 +1,19 @@
 # awaybar.pl -- initially built for Irssi 0.8.9
-# thanks to mood.pl for practically allowing me
-# to copy the approach..
+# thanks to mood.pl for practically allowing me to copy the approach..
+#
+# Usage:
+#
+# Creating the away bar
+# This assumes you have adv_windowlist, and that we add before it's window named 'awl_0'
+# /set awl_shared_sbar RIGHT
+# /statusbar window add -after lag -priority 10 -alignment right awl_shared
+# /sbar awl_shared add -before awl_0 -alignment right awaybar
+#
+# Appearance
+# To set the look and feel of the awaybar, and save it to your .theme file:
+# /format sb_awaybar "%1%Waway: ${0-}"
+# /save
+
 use strict;
 use Irssi;
 use Irssi::TextUI;
@@ -28,8 +41,7 @@ sub awaybar {
                  '%' => '%%',);
         $away_reason =~ s/$_/$r{$_}/g for (keys %r);
 
-        #my $format = $theme->format_expand("{sb_awaybar $away_reason}");
-        my $format = "{sb Away: $away_reason}";
+        my $format = "{sb_awaybar $away_reason}";
 
         $item->{min_size} = $item->{max_size} = length($away_reason);
         $item->default_handler($get_size_only, $format, 0, 1);
