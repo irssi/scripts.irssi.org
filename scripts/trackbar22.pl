f5a108b97e30d0744182d4de4555e37c9cb28b4e
Ailin Nemui <ailin@z30a.localdomain>
2018-02-01 16:16:29 +0100

    add dummy trackbar22 script

---
+++ scripts/trackbar22.pl
@@ -0,0 +1,13 @@
+use strict;
+use warnings;
+
+use vars qw($VERSION %IRSSI);
+
+$VERSION = '2.5';
+
+%IRSSI = (
+    description => 'This script has been replaced with trackbar.pl.',
+    HIDDEN => 1
+   );
+
+die "$IRSSI{description} Please run /script install trackbar and manually remove this script. Sorry for the inconvenience.\n";
