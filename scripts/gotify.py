17839dad1295819946c9abcc753ac783b75f553c
terminaldweller <devi@terminaldweller.com>
2023-09-25 11:08:35 -0400

    changed key to authors to be picked up by the website

--- scripts/gotify.py
+++ scripts/gotify.py
@@ -4,7 +4,7 @@ from urllib import request, parse
 __version__ = "0.1.0"
 
 IRSSI = {
-    "author": "terminaldweller",
+    "authors": "terminaldweller",
     "contact": "https://terminaldweller.com",
     "name": "gotify",
     "description": "sends push notifications via gotify",
